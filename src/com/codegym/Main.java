659fd8d9fc23cc5668a6d9f6d0fbb49d9142ec59
DuongXuanBach <duongxuanbach3112@gmail.com>
2022-02-08 16:42:00 +0700

    add file

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -25,9 +25,36 @@ public class Main {
                     }
                     break;
                 case 2:
-                    System.out.println("Print the square triangle");
+                    System.out.println("Print the square triangle( bottom-left)");
                     for (int i = 1; i <= 7 ; i++) {
                         for (int j = 1; j < i ; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("Print the square triangle(top-left)");
+                    for (int i = 6; i >= 1 ; i--) {
+                        for (int j = 1; j <= i ; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("Print the square triangle( top-right)");
+                    for (int i = 0; i<= 6; i++) {
+                        for (int j = 0; j < i ; j++) {
+                            System.out.print(" ");
+                        }
+                        for(int j = 1; j <= 6 - i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("Print the square triangle( bottom-right)");
+                    for (int i = 1; i <= 6; i++) {
+                        for (int j = 0; j < 6 - i; j++) {
+                            System.out.print(" ");
+                        }
+                        for (int j = 1; j <= i; j++) {
                             System.out.print("*");
                         }
                         System.out.println();
@@ -35,9 +62,14 @@ public class Main {
                     break;
                 case 3:
                     System.out.println("Print isosceles triangle");
-                    for (int i = 7; i >= 1 ; i--) {
-                        for (int j = 1; j <= i ; j++) {
-                            System.out.print("*");
+                    int k = 0;
+                    for (int i = 1; i <= 4; ++i , k = 0) {
+                        for (int j = 1; j <= 4 - i; ++j) {
+                            System.out.print("  ");
+                        }
+                        while (k != 2 * i - 1) {
+                            System.out.print("* ");
+                            ++k;
                         }
                         System.out.println();
                     }
